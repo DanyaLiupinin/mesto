73dc9ef9221ad82e3adfb6870ef791113b7d1949
DanyaLiupinin <mr.kovan_2002@mail.ru>
2022-04-22 14:26:44 +0300

    2nd version

--- scripts/index.js
+++ scripts/index.js
@@ -4,9 +4,19 @@ const profileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close-button');
 
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+let inputName = popup.querySelector('.popup__input_content_name');
+let inputDescription = popup.querySelector('.popup__input_content_description');
+
+
 /* функция добавляет и убирает класс popup_opened */
 function popupOpen () {
    popup.classList.toggle('popup_opened');
+   if (popup.classList.contains('popup_opened')) {
+      inputName.value = profileName.textContent;
+      inputDescription.value = profileDescription.textContent;
+   }
 }
 
  /* клик вызывает функцию popupOpen */
@@ -15,10 +25,30 @@ profileEditButton.addEventListener('click', popupOpen);
 /* клик по крестику снова вызывает функцию popupOpen */
 popupCloseButton.addEventListener('click', popupOpen);
 
-/* при клике на всю область попапа вызывает функцию ниже */
+
+/* сохранение данных в профиле */
+
+const ProfileForm = popup.querySelector('.popup__form');
+
+
+ProfileForm.addEventListener('submit', saveProfileInfo);
+
+function saveProfileInfo (form) {
+   form.preventDefault();
+   profileName.textContent = inputName.value;
+   profileDescription.textContent = inputDescription.value;
+   popupOpen ();
+}
+
+
+
+
+
+/*
+// при клике на всю область попапа вызывает функцию ниже //
 popup.addEventListener('click', popupClickOverlay);
 
-/* если клик по попапу (а не по детям), то срабатывает функция popupOpen; в ином случае ничего не происходит */
+// если клик по попапу (а не по детям), то срабатывает функция popupOpen; в ином случае ничего не происходит //
 function popupClickOverlay (evt) {
    if (evt.target === evt.currentTarget) {
       popupOpen ();
@@ -26,19 +56,4 @@ function popupClickOverlay (evt) {
       return;
    }
 }
-
-/* сохранение данных в профиле */
-
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');
-let inputName = popup.querySelector('.popup__input_name');
-let inputDescription = popup.querySelector('.popup__input_description');
-const saveProfileInfoButton = popup.querySelector('.popup__submit-button');
-
-saveProfileInfoButton.addEventListener('click', saveProfileInfo);
-
-function saveProfileInfo () {
-   profileName.textContent = inputName.value;
-   profileDescription.textContent = inputDescription.value;
-   popupOpen ();
-}
+*/
