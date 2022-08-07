743219a6310b144b02627759f529d8fee9a8f636
Danya Kovan <mr.kovan_2002@mail.ru>
2022-08-07 12:39:23 +0300

    -folder scripts

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,14 @@ import {
   formAddCard,
   formProfile,
   cardsContainer,
-} from '../scripts/utils/constants.js'
+} from '../utils/constants.js'
 import './index.css'
-import { Card } from '../scripts/components/Card.js' 
-import { FormValidator } from '../scripts/components/FormValidator.js'
-import { Section } from '../scripts/components/Section.js'
-import { PopupWithForm } from '../scripts/components/PopupWithForm.js'
-import { PopupWithImage } from '../scripts/components/PopupWithImage.js'
-import { UserInfo } from '../scripts/components/UserInfo.js'
+import { Card } from '../components/Card.js' 
+import { FormValidator } from '../components/FormValidator.js'
+import { Section } from '../components/Section.js'
+import { PopupWithForm } from '../components/PopupWithForm.js'
+import { PopupWithImage } from '../components/PopupWithImage.js'
+import { UserInfo } from '../components/UserInfo.js'
 
 
 // экземпляр валидатора 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,15 +1,15 @@
 //картинки из html
 
-const avatar = new URL ('../../images/avatar.jpg', import.meta.url)
-const closeButton = new URL ('../../images/close-button.svg', import.meta.url)
-const deleteButton = new URL ('../../images/delete-button.svg', import.meta.url)
-const headerLogo = new URL ('../../images/header__logo.svg', import.meta.url)
-const likeActive = new URL ('../../images/like_active.svg', import.meta.url)
-const like = new URL ('../../images/like.svg', import.meta.url)
-const plus = new URL ('../../images/plus.svg', import.meta.url)
-const addButtonAddaptive = new URL ('../../images/profile__add-button-addaptive.svg', import.meta.url)
-const addButton = new URL ('../../images/profile__add-button.svg', import.meta.url)
-const editButton = new URL ('../../images/profile__editbutton.svg', import.meta.url)
+const avatar = new URL ('../images/avatar.jpg', import.meta.url)
+const closeButton = new URL ('../images/close-button.svg', import.meta.url)
+const deleteButton = new URL ('../images/delete-button.svg', import.meta.url)
+const headerLogo = new URL ('../images/header__logo.svg', import.meta.url)
+const likeActive = new URL ('../images/like_active.svg', import.meta.url)
+const like = new URL ('../images/like.svg', import.meta.url)
+const plus = new URL ('../images/plus.svg', import.meta.url)
+const addButtonAddaptive = new URL ('../images/profile__add-button-addaptive.svg', import.meta.url)
+const addButton = new URL ('../images/profile__add-button.svg', import.meta.url)
+const editButton = new URL ('../images/profile__editbutton.svg', import.meta.url)
 
 export const pictures = [
 {name: 'avatar', link: avatar},
@@ -50,12 +50,12 @@ export const cardsContainer = document.querySelector('.elements');
 
 // массив карточек в профиле // 
 
-const imageCardOne = new URL ('../../images/pf.jpg', import.meta.url) //без этого карточки будут добавляться в dist, но не будут отображаться на странице
-const imageCardTwo = new URL ('../../images/ps.jpg', import.meta.url)
-const imageCardThree = new URL ('../../images/p3.jpg', import.meta.url)
-const imageCardFour = new URL ('../../images/p4.jpg', import.meta.url)
-const imageCardFive = new URL ('../../images/p5.jpg', import.meta.url)
-const imageCardSix = new URL ('../../images/p6.jpg', import.meta.url)
+const imageCardOne = new URL ('../images/pf.jpg', import.meta.url) //без этого карточки будут добавляться в dist, но не будут отображаться на странице
+const imageCardTwo = new URL ('../images/ps.jpg', import.meta.url)
+const imageCardThree = new URL ('../images/p3.jpg', import.meta.url)
+const imageCardFour = new URL ('../images/p4.jpg', import.meta.url)
+const imageCardFive = new URL ('../images/p5.jpg', import.meta.url)
+const imageCardSix = new URL ('../images/p6.jpg', import.meta.url)
 
 export const cards = [
     {
